d36d934888ef603ef7e359a39e8806f385426014
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-06-20 15:37:33 -0400

    added ng-submit to all of the form fields. adds Mutant contructor to the controller.

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -1,14 +1,32 @@
 (function() {
   'use strict';
-  
+
   angular
     .module('mutantApp.mutantList')
     .controller('MutantListController', MutantListController);
 
   function MutantListController() {
     var vm = this;
+    //vm is the alias for the controller
 
+    vm.addMutant = addMutant;
     vm.mutants = ['deadpool', 'nightcrawler', 'gambit'];
+
+    vm.newMutant = new Mutant();
+
+  function Mutant() {
+    this.name = '';
+    this.phone='';
+    this.topic = '';
+    this.notified = false;
+    this.compelte = false;
   }
+
+    function addMutant() {
+      vm.mutants.push(vm.newMutant);
+    }
+
+  }
+
 
 })();
