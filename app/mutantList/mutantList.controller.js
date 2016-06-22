056a58ff83da7ffb32561a878f937c025fa2162e
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-06-22 11:25:30 -0400

    Added some documentation to the functions.

--- app/mutantList/mutantList.controller.js
+++ app/mutantList/mutantList.controller.js
@@ -20,7 +20,7 @@
     vm.toggleComplete = toggleComplete;
     vm.sendText = sendText;
 
-
+      // Mutant Constructor
   function Mutant() {
     this.name = '';
     this.phone='';
@@ -29,18 +29,25 @@
     this.compelte = false;
   }
 
+    // This function takes adds a mutant to the database from the text typed in the input boxes
   function addMutant() {
-    vm.mutants.$add(vm.newMutant); //$add will push to our local copy and to the database
+    vm.mutants.$add(vm.newMutant); //$add will a new object to the local copy and to the database
     vm.newMutant = new Mutant();
   }
 
+    // This function takes a single mutant as a parameter and removes it from the database
   function deleteMutant(mutant) {
-    vm.mutants.$remove(mutant); // list of mutants and delete this specific mutant
+    vm.mutants.$remove(mutant);
   }
 
+    // This function takes a single mutant as a parameter and will save it to the database
   function toggleComplete(mutant) {
     vm.mutants.$save(mutant); // just need to save it to the database because it already is saved locally
   }
+
+    // This function takes a signle mutant as a parameter, and creates a newText object using the mutants,
+    // name, topic, and phone number. Then the newText object is pushed to firebase, setting notified to true
+    // and then saving the mutant to the database.
   function sendText(mutant) {
     var newText = {
       name: mutant.name,
