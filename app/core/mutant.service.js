2a21d90362c41879592cc568266c0f1e112457a1
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-06-23 13:34:33 -0400

    Set up mutantService skeleton factory.

---
+++ app/core/mutant.service.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('mutantApp.core')
+    .factory('mutantService', mutantService);
+
+
+    function mutantService() {
+      var service = {};
+      
+      return service;
+    }
+
+})();
