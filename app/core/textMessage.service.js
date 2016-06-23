bfceb2d4e700043197cd037934f6820412694e43
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-06-23 14:05:53 -0400

    Set up extMessageService and added it to the index.html.

---
+++ app/core/textMessage.service.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('mutantApp.core')
+    .factory('textMessageService', textMessageService);
+
+
+    function textMessageService() {
+      var service = {};
+
+      return service;
+
+    }
+
+})();
